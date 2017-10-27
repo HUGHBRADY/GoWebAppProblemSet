eba2f84af7b39143ebb28189acca3c827e3c470f
Hugh <Hugh>
2017-10-27 22:48:56 +0100

    Task 3 completed; guess.html, CSS and JS added to repo

--- GuessingGame.go
+++ GuessingGame.go
@@ -1,13 +1,19 @@
 // Web Application - Guessing Game
 // Hugh Brady - G00338260 - 23/10/17
 
-package main 
+package main
 
 import "net/http"
+
+func guessHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "/guess.html")
+}
 
 func main() { 
 	// handles request for index.html by serving files in "static" folder
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	http.HandleFunc("/guess", guessHandler)
 
 	// serves on local host
     http.ListenAndServe(":8080", nil)
